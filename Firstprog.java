12745476778b9d41c646723d4c3e7c29b0199013
Bhavnoor Singh <bhav27noor@gmail.com>
2020-09-04 21:23:57 +0530

    First Program Basic

---
+++ Firstprog.java
@@ -0,0 +1,18 @@
+package Coding_Blocks;
+
+public class Firstprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		func();
+		System.out.println("Bye");
+	}
+	public static void func()
+	{
+		int a=10;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
