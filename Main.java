4434e4dbb454663edb4c54b89e2e8084c9968082
Bhavnoor Singh <bhav27noor@gmail.com>
2020-09-17 13:35:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+	
+    for(int i=1; i<=4; i++)
+    {
+    	System.out.println("Hello "+ i);
+    	if(i==3)
+    	{
+    		break;
+    	}
+    	System.out.println("Done " + i);
+    }
+    }
+}
